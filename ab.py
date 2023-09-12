ffe1ab4fb8590ffa98d94057f6259eafc723ead0
bhaskar <bhaskard1307@gmail.com>
2023-09-12 14:09:08 +0530

    my first program

---
+++ ab.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+plt.rcParams['figure.figsize'] = (20.0, 10.0)
+# Reading Data
+data = pd.read_csv('headbrain.csv')
+print(data.shape)
+data.head()
+ # Collecting X and Y
+X = data['Head Size(cm^3)'].values
+Y = data['Brain Weight(grams)'].values
+# Mean X and Y
+mean_x = np.mean(X)
+mean_y = np.mean(Y)
+ # Total number of values
+n = len(X)
+ # Using the formula to calculate m and c
+numer = 0
+denom = 0
+for i in range(n):
+    numer += (X[i] - mean_x) * (Y[i] - mean_y)
+    denom += (X[i] - mean_x) ** 2
+    m = numer / denom
+    c = mean_y - (m * mean_x)
+ 
+# Print coefficients
+print(m, c)
